697880506925626eb387fa46b13a9bdcdfcac470
shaikhtanveer811 <30755794+shaikhtanveer811@users.noreply.github.com>
2017-08-08 15:47:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,25 @@
-console.log('Loaded!');
+// Counter code
+var button = document.getElementById('counter');
 
-// Change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New value';
-
-// Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
+button.onclick = function () {
+    
+    //Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a vairable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://shaikhtanveer811.imad.hasura-app.io/counter', true);
+    request.send(null);
 }
-img.onclick = function () {
-  var interval = setInterval(moveRight, 50);
-};
