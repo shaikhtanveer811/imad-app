6f94a38d13576970f5ad358885b4fb1429eb1ba9
shaikhtanveer811 <30755794+shaikhtanveer811@users.noreply.github.com>
2017-08-08 21:59:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,9 +111,9 @@ app.get('/ui/madi.png', function (req, res) {
 // Use 8080 only for local development if you already have apache running on 80
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) {
+app.get('/submit-name', function(req, res) { // URL: /submit-nae?name=xxxxx
    // Get the name from the request 
-   var name = req.params.name;
+   var name = req.query.name;
    
    names.push(name);
    // JSON: Javascript Object Notation
