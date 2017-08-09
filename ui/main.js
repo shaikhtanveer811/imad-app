db37a4297d3602d314afb8490e67bd82736f4ef3
shaikhtanveer811 <30755794+shaikhtanveer811@users.noreply.github.com>
2017-08-09 20:00:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick = function () {
     };
     
     // Make the request
-    request.open('GET', 'http://shaikhtanveer811.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.open('GET', 'http://shaikhtanveer811.imad.hasura-app.io/counter', true);
     request.send(null);
 };
 
