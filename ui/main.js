62694dd114f35f164d421d21ca87046a588cca21
shaikhtanveer811 <30755794+shaikhtanveer811@users.noreply.github.com>
2017-08-09 19:58:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,31 @@ button.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             // Take some action
             if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://shaikhtanveer811.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.send(null);
+};
+
+// Submit name
+var submit = document.getElementbyId('submit_btn');
+submit.onclick = function () {
+    
+ //Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a vairable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200) {
                 // Capture a list of names and render it as a list
                 var names = request.responseText;
                 names = JSON.parse(names);
@@ -30,10 +55,4 @@ button.onclick = function () {
     var name = nameInput.value;
     request.open('GET', 'http://shaikhtanveer811.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
-};
-
-// Submit name
-var submit = document.getElementbyId('submit_btn');
-submit.onclick = function () {
-  // Make a request to the server and send the name
 };
