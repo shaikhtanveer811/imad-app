543f2e37cab69fb56b146819a921745d533c2ca8
shaikhtanveer811 <30755794+shaikhtanveer811@users.noreply.github.com>
2017-08-10 14:34:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+
+var config = {
+    user: 'shaikhtanveer811',
+    database: 'shaikhtanveer811',
+    host: 'db_imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -81,6 +90,16 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/test-db', function (req, res) {
+   pool.query('SELECT * FROM test', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      }  else {
+          res.send(JSON.stringify(result));
+      }
+   }); 
 });
 
 var counter = 0;
